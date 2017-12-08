d554ea83652f700883bcbae994b161f3f8712188
Jan Lehr <jp.lehr@gmail.com>
2017-12-08 12:57:38 +0100

    Added code from jplehr/llvm-memprofiler to the project

--- lib/TypeUtil.cpp
+++ lib/TypeUtil.cpp
@@ -6,22 +6,75 @@
 #include "llvm/IR/Instructions.h"
 
 
+#include <iostream>
+
 using namespace llvm;
 
 namespace util {
 	namespace type {
 
-int getTypeSizeInBytes(llvm::Type *t, const llvm::DataLayout &dl){}
+		/**
+		 * Code was imported from jplehr/llvm-memprofiler project
+		 */
+		int getTypeSizeInBytes(llvm::Type *t, const llvm::DataLayout &dl){
+			int bytes = getScalarSizeInBytes(t);
 
-int getScalarSizeInBytes(llvm::Type *t){}
+			if (t->isArrayTy()) {
+				bytes = getArraySizeInBytes(t, dl);
+			} else if (t->isStructTy()) {
+				bytes = getStructSizeInBytes(t, dl);
+			} else if (t->isPointerTy()) {
+				bytes = getPointerSizeInBytes(t, dl);
+			}
 
-int getArraySizeInBytes(llvm::Type *arrT, const llvm::DataLayout &dl){}
+			return bytes;
+		}
 
-int getStructSizeInBytes(llvm::Type *structT, const llvm::DataLayout &dl){}
+		int getScalarSizeInBytes(llvm::Type *t){
+			return t->getScalarSizeInBits() / 8; 	
+		}
 
-int getPointerSizeInBytes(llvm::Type *ptrT, const llvm::DataLayout &dl){}
+		int getArraySizeInBytes(llvm::Type *arrT, const llvm::DataLayout &dl){
+			auto st = dyn_cast<ArrayType>(arrT);
+			Type *underlyingType = st->getElementType();
+			int bytes = getScalarSizeInBytes(underlyingType);
+			bytes *= st->getNumElements();
+			std::cout << "Determined number of bytes to allocate: " << bytes << std::endl;
 
-int getTypeSizeForArrayAlloc(llvm::AllocaInst *ai, const llvm::DataLayout &dl){}
+			return bytes;
+		}
+
+		int getStructSizeInBytes(llvm::Type *structT, const llvm::DataLayout &dl){
+			int bytes = 0;
+			for (auto it = structT->subtype_begin(); it != structT->subtype_end(); ++it) {
+				bytes += getTypeSizeInBytes(*it, dl);
+			}
+			return bytes;
+		}
+
+		int getPointerSizeInBytes(llvm::Type *ptrT, const llvm::DataLayout &dl){
+			return dl.getPointerSizeInBits() / 8;
+		}
+
+		int getTypeSizeForArrayAlloc(llvm::AllocaInst *ai, const llvm::DataLayout &dl){
+			int bytes = ai->getAllocatedType()->getScalarSizeInBits() / 8;
+			if (ai->isArrayAllocation()) {
+				if (auto ci = dyn_cast<ConstantInt>(ai->getArraySize())) {
+					bytes *= ci->getLimitedValue();
+				} else {
+					// If this can not be determined statically, we have to compute it at
+					// runtime. We insert additional instructions to calculate the
+					// numBytes of that array on the fly. (VLAs produce this kind of
+					// behavior)
+					// ATTENTION: We can have multiple such arrays in a single BB. We need
+					// to have a small vector to store whether we already generated
+					// instructions, to possibly refer to the results for further
+					// calculations.
+					std::cout << "We hit not yet determinable array size expression\n";
+				}
+			}
+			return bytes;
+		}
 
 	} // type
 } // util 
