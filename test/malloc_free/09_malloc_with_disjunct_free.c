5859bf876eba186dac4f1e4352052ae3730b7919
Alexander Hück <hueck.alexander@gmail.com>
2018-01-05 18:24:21 +0100

    fix test

--- test/malloc_free/09_malloc_with_disjunct_free.c
+++ test/malloc_free/09_malloc_with_disjunct_free.c
@@ -1,7 +1,11 @@
 // RUN: clang -S -emit-llvm %s -o - | opt -load %pluginpath/%pluginname %pluginargs -S 2>&1 | FileCheck %s
 #include <stdlib.h>
+
+void foo(double* ptr);
+
 void test() {
   double* p = (double*)malloc(42 * sizeof(double));
+  foo(p);
 }
 
 void foo(double* ptr) {
