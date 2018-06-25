900a9f1ea7b71632566a4ad1df4fc583d710d0b7
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2018-06-25 22:23:27 +0200

    Add missing file

---
+++ runtime/RuntimeUtil.h
@@ -0,0 +1,13 @@
+#ifndef TYPEART_RUNTIMEUTIL_H
+#define TYPEART_RUNTIMEUTIL_H
+
+namespace typeart {
+
+template<typename T>
+inline const void* addByteOffset(const void* addr, T offset) {
+    return static_cast<const void*>(static_cast<const uint8_t*>(addr) + offset);
+}
+
+}
+
+#endif //TYPEART_RUNTIMEUTIL_H
