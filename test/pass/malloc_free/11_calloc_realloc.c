6073c506c13e214dae7a0a50ff227fe9d9b94555
Alexander Hück <hueck.alexander@gmail.com>
2018-07-11 19:35:38 +0200

    unit test for calloc and realloc

--- test/pass/malloc_free/11_calloc_realloc.c
+++ test/pass/malloc_free/11_calloc_realloc.c
@@ -1,12 +1,15 @@
-//run: FileCheck %s
-//XFAIL: *
-#include "stdlib.h"
+// clang-format off
+// RUN: clang -S -emit-llvm %s -o - | opt -load %pluginpath/analysis/meminstfinderpass.so -load %pluginpath/%pluginname %pluginargs -S 2>&1 | FileCheck %s
+// clang-format on
+#include <stdlib.h>
 
 int main() {
+  double* pd = calloc(10, sizeof(double));
 
-	double *pd = calloc(10, sizeof(double));
+  pd = realloc(pd, 20 * sizeof(double));
 
-	pd = realloc(pd, 20 * sizeof(double));
-
-	return 0;
+  return 0;
 }
+
+// CHECK: Malloc{{[ ]*}}:{{[ ]*}}2
+// CHECK: Alloca{{[ ]*}}:{{[ ]*}}0
