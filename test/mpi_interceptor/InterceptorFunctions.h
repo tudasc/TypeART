d9a9c1584583c56f5bf8c5e7cda2f538b682964f
Alexander Hück <hueck.alexander@gmail.com>
2018-07-16 18:06:32 +0200

    track origin of MPI call

--- test/mpi_interceptor/InterceptorFunctions.h
+++ test/mpi_interceptor/InterceptorFunctions.h
@@ -11,30 +11,55 @@
 #include "RuntimeInterface.h"
 
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void ta_check_const_void(const void* buf, MPI_Datatype dtype) {
-  int rank = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  typeart_type_info type;
-  size_t count = 0;
-  typeart_status typeart_status_v = typeart_get_type(buf, &type, &count);
-  if (typeart_status_v != TA_OK) {
-    printf("R[%d][Error] Input buffer %p status: %d\n", rank, buf, (int)typeart_status_v);
-  }
-  //  else {
-  //    printf("R[%d][OK] Input buffer %p status: OK\n", rank, buf);
-  //  }
+int ta_check_exists(const char* mpi_name, const void* called_from, const void* buf);
+void ta_print_loc(const void* call_adr);
+
+void ta_check_const_void(const char* name, const void* called_from, const void* buf, MPI_Datatype dtype) {
+  ta_check_exists(name, called_from, buf);
 }
 
-void ta_check_void(const void* buf) {
+void ta_check_void(const char* name, const void* called_from, const void* buf) {
+  ta_check_exists(name, called_from, buf);
+}
+
+int ta_check_exists(const char* mpi_name, const void* called_from, const void* buf) {
+  if (buf == NULL) {
+    return -1;
+  }
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   typeart_type_info type;
   size_t count = 0;
   typeart_status typeart_status_v = typeart_get_type(buf, &type, &count);
-  if (typeart_status_v != TA_OK) {
-    printf("R[%d][Error] Output buffer buffer %p status: %d\n", rank, buf, (int)typeart_status_v);
+  if (typeart_status_v != TA_OK && rank == 0) {
+    printf("R[%d][Error] Call '%s' buffer %p at loc %p status: %d\n", rank, mpi_name, buf, called_from,
+           (int)typeart_status_v);
+    ta_print_loc(called_from);
+    return 0;
   }
+  return 1;
+}
+
+void ta_print_loc(const void* call_adr) {
+  const char* exe = getenv("TA_EXE_TARGET");
+  if (exe == NULL || exe[0] == '\0') {
+    return;
+  }
+
+  char cmd_buf[512];
+  snprintf(cmd_buf, sizeof(cmd_buf), "addr2line -e %s -f %p", exe, call_adr);
+
+  FILE* fp = popen(cmd_buf, "r");
+  if (fp) {
+    char read_buf[512];
+    while (fgets(read_buf, sizeof(read_buf), fp)) {
+      printf("    %s", read_buf);
+    }
+  }
+  fclose(fp);
 }
 
 #endif /* TEST_MPI_INTERCEPTOR_INTERCEPTORFUNCTIONS_H_ */
