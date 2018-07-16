4c9361b736b5b13615d9d547e6506e1a9bf2f7b9
Alexander Hück <hueck.alexander@gmail.com>
2018-07-16 16:10:31 +0200

    finish interceptor lib

--- test/mpi_interceptor/InterceptorFunctions.h
+++ test/mpi_interceptor/InterceptorFunctions.h
@@ -13,17 +13,28 @@
 #include <mpi.h>
 
 void ta_check_const_void(const void* buf, MPI_Datatype dtype) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   typeart_type_info type;
   size_t count = 0;
   typeart_status typeart_status_v = typeart_get_type(buf, &type, &count);
   if (typeart_status_v != TA_OK) {
-    int rank = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    printf("R[%d][Error] Input buffer status: %d\n", rank, (int)typeart_status_v);
+    printf("R[%d][Error] Input buffer %p status: %d\n", rank, buf, (int)typeart_status_v);
   }
+  //  else {
+  //    printf("R[%d][OK] Input buffer %p status: OK\n", rank, buf);
+  //  }
 }
 
 void ta_check_void(const void* buf) {
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  typeart_type_info type;
+  size_t count = 0;
+  typeart_status typeart_status_v = typeart_get_type(buf, &type, &count);
+  if (typeart_status_v != TA_OK) {
+    printf("R[%d][Error] Output buffer buffer %p status: %d\n", rank, buf, (int)typeart_status_v);
+  }
 }
 
 #endif /* TEST_MPI_INTERCEPTOR_INTERCEPTORFUNCTIONS_H_ */
