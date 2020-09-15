c62bb4a9af04605c501d8d8bd48f79d8e0a431a8
Alexander Hück <hueck.alexander@gmail.com>
2020-09-15 15:29:32 +0200

    test folder for cpp specific tests

--- test/pass/new_delete/04_inv_struct_array.cpp
+++ test/pass/new_delete/04_inv_struct_array.cpp
@@ -1,18 +1,25 @@
 // clang-format off
 // RUN: clang++ -S -emit-llvm %s -o - | opt -load %pluginpath/analysis/meminstfinderpass.so -load %pluginpath/%pluginname %pluginargs -S 2>&1 | FileCheck %s
-// fails at two points, TA callback size is wrongly calculated, is the correct poiter used?
+// XFAIL: *
+// Fix: size is calculated based on new call with array cookie; address is array cookie
 // clang-format on
+
+#include <new>
 
 struct S1 {
   int x;
-  ~S1() { };
+  virtual ~S1() = default;
 };
 
-// CHECK: call i8* @_Znam(i64 8)
-// CHECK: call void @__typeart_alloc(i8* [[POINTER:%[0-9]+]], i32 {{2[0-9]+}}, i64 2)
+// CHECK: invoke i8* @_Znam(i64 40)
+// CHECK: call void @__typeart_alloc(i8* [[POINTER:%[0-9]+]], i32 {{2[5-9][0-9]}}, i64 2)
 // CHECK: bitcast i8* [[POINTER]] to %struct.S1*
 int main() {
-  S1* ss = new S1[2];
+  try {
+    S1* ss = new S1[2];
+  } catch (...) {
+  }
+
   return 0;
 }
 
