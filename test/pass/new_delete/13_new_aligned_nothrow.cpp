cf9a9478703a16955d5d94edf0d087671ba36e25
Alexander Hück <hueck.alexander@gmail.com>
2020-12-10 14:03:38 +0100

    add aligned new test

--- test/pass/new_delete/13_new_aligned_nothrow.cpp
+++ test/pass/new_delete/13_new_aligned_nothrow.cpp
@@ -2,6 +2,7 @@
 // RUN: %cpp-to-llvm %s | %apply-typeart -S 2>&1 | FileCheck %s
 // clang-format on
 
+#define __cpp_aligned_new 1
 #include <new>
 
 struct C {
@@ -9,15 +10,15 @@ struct C {
 };
 
 C* foo() {
-  return new (std::nothrow) C[10];
+  return new (std::align_val_t(64), std::nothrow) C[10];
 }
 
 C* bar() {
-  return new (std::nothrow) C;
+  return new (std::align_val_t(128), std::nothrow) C;
 }
 
-// CHECK: [[POINTER:%[0-9]+]] = call noalias i8* @_ZnamRKSt9nothrow_t(i64 40,
+// CHECK: [[POINTER:%[0-9]+]] = call noalias i8* @_ZnamSt11align_val_tRKSt9nothrow_t(i64 40,
 // CHECK-NEXT: call void @__typeart_alloc(i8* [[POINTER]], i32 [[ID:2[5-9][0-9]]], i64 10)
 
-// CHECK: [[POINTER2:%[0-9]+]] = call noalias i8* @_ZnwmRKSt9nothrow_t(i64 4,
+// CHECK: [[POINTER2:%[0-9]+]] = call noalias i8* @_ZnwmSt11align_val_tRKSt9nothrow_t(i64 4,
 // CHECK-NEXT: call void @__typeart_alloc(i8* [[POINTER2]], i32 [[ID]], i64 1)
