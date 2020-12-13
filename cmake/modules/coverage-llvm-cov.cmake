5176d67e15fc917a1cdbcd2b5380aa700d40e448
Alexander Hück <hueck.alexander@gmail.com>
2020-12-13 20:40:38 +0100

    llvm-cov (and overall workflow) progress

--- cmake/modules/coverage-llvm-cov.cmake
+++ cmake/modules/coverage-llvm-cov.cmake
@@ -2,3 +2,43 @@ set(TYPEART_PROFILE_DIR ${CMAKE_BINARY_DIR}/profiles)
 
 file(MAKE_DIRECTORY ${TYPEART_PROFILE_DIR})
 
+add_custom_target(
+  cov-merge
+  COMMAND llvm-profdata merge -sparse -o code.pro *.profraw
+  DEPENDS ${target}
+  WORKING_DIRECTORY ${TYPEART_PROFILE_DIR}
+  DEPENDS ${target}
+)
+
+add_custom_target(
+  cov-all-report
+  COMMAND echo ${SO_OBJECTS}
+  COMMAND llvm-cov report `cat -s ${TYPEART_PROFILE_DIR}/ta-binaries.txt` --instr-profile=${TYPEART_PROFILE_DIR}/code.pro
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+  DEPENDS cov-merge
+)
+
+function(make_llvm_cov_target target)
+  add_custom_target(
+    cov-binary-list-${target}
+    COMMAND ${CMAKE_COMMAND} -E echo "-object $<TARGET_FILE:${target}>" >> ${TYPEART_PROFILE_DIR}/ta-binaries.txt
+    DEPENDS ${target}
+  )
+
+  add_custom_target(
+    cov-merge-${target}
+    COMMAND llvm-profdata merge -sparse -o code-${target}.pro *.profraw
+    DEPENDS ${target}
+    WORKING_DIRECTORY ${TYPEART_PROFILE_DIR}
+    DEPENDS ${target}
+  )
+
+  add_custom_target(
+    cov-report-${target}
+    COMMAND llvm-cov report -object $<TARGET_FILE:${target}> --instr-profile=${TYPEART_PROFILE_DIR}/code-${target}.pro
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    DEPENDS ${target} cov-merge-${target}
+  )
+
+  add_dependencies(cov-all-report cov-binary-list-${target})
+endfunction()
--- cmake/modules/coverage.cmake
+++ cmake/modules/coverage.cmake
@@ -1,3 +1,6 @@
+include(coverage-gcovr)
+include(coverage-llvm-cov)
+
 function(target_project_coverage_options target)
   if (ENABLE_CODE_COVERAGE)
     if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
@@ -7,16 +10,15 @@ function(target_project_coverage_options target)
     target_link_options(${target} PUBLIC --coverage)
     endif ()
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-      target_compile_options(${target} INTERFACE
+      target_compile_options(${target} PUBLIC
         -O0 -g -fprofile-instr-generate -fcoverage-mapping
         )
-      target_link_options(${target} INTERFACE -fprofile-instr-generate)
+      target_link_options(${target} PUBLIC -fprofile-instr-generate)
+
+      make_llvm_cov_target(${target})
     endif ()
   endif()
 endfunction()
-
-include(coverage-gcovr)
-include(coverage-llvm-cov)
 
 
 
