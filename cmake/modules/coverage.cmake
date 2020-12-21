90b6b219b2eb90d7ff463085d497939c5914ad31
Alexander Hück <hueck.alexander@gmail.com>
2020-12-21 18:04:16 +0100

    some conditions for coverage make targets

--- cmake/modules/coverage.cmake
+++ cmake/modules/coverage.cmake
@@ -1,12 +1,17 @@
 set(TYPEART_PROFILE_DIR ${CMAKE_BINARY_DIR}/profiles)
 file(MAKE_DIRECTORY ${TYPEART_PROFILE_DIR})
 
-include(coverage-gcovr)
-include(coverage-lcov)
-include(coverage-llvm-cov)
+if(NOT ENABLE_LLVM_CODE_COVERAGE AND ENABLE_CODE_COVERAGE)
+  include(coverage-gcovr)
+  include(coverage-lcov)
+endif()
+
+if(ENABLE_LLVM_CODE_COVERAGE)
+  include(coverage-llvm-cov)
+endif()
 
 function(target_project_coverage_options target)
-  if (ENABLE_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+  if (NOT ENABLE_LLVM_CODE_COVERAGE AND ENABLE_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     target_compile_options(${target} PUBLIC
       -O0
       -g
@@ -23,11 +28,10 @@ function(target_project_coverage_options target)
       -g
       -fprofile-instr-generate
       -fcoverage-mapping
-      )
+    )
     target_link_options(${target} PUBLIC
       -fprofile-instr-generate
-      )
-
+    )
     make_llvm_cov_target(${target})
   endif ()
 endfunction()
