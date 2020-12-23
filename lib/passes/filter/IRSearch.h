de1c26d3efb7b18a1817bd5b4cdd1d93f19c5635
Alexander Hück <hueck.alexander@gmail.com>
2020-12-23 20:14:35 +0100

    nrvo in DefaultSearch

--- lib/passes/filter/IRSearch.h
+++ lib/passes/filter/IRSearch.h
@@ -22,12 +22,12 @@ struct DefaultSearch {
       if (llvm::isa<AllocaInst>(val) && !store->getValueOperand()->getType()->isPointerTy()) {
         // 1. if we store to an alloca, and the value is not a pointer (i.e., a value) there is no connection to follow
         // w.r.t. dataflow. (TODO exceptions could be some pointer arithm.)
-        return {};
+        return out;
       }
       if (p.contains(val)) {
         // If the pointer operand is already in the path, we do not want to continue.
         // Encountered: amg: box_algebra.c: hypre_MinUnionBoxes endless recursion
-        return {};
+        return out;
       }
 
       out.push_back(val);
