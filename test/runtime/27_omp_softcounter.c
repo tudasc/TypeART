89e5a00d5c3cb8766d460bf015ccfccb433095f3
Alexander Hück <hueck.alexander@gmail.com>
2021-01-28 14:44:52 +0100

    use call-filter in test 27

--- test/runtime/27_omp_softcounter.c
+++ test/runtime/27_omp_softcounter.c
@@ -1,5 +1,6 @@
 // clang-format off
-// RUN: %run %s -o -O2 --omp 2>&1 | FileCheck %s
+// RUN: %run %s -o -O2 --omp --call-filter 2>&1 | FileCheck %s
+// RUN: %run %s --omp --call-filter 2>&1 | FileCheck %s
 // REQUIRES: openmp && softcounter
 // clang-format on
 
@@ -30,18 +31,18 @@ int main(int argc, char** argv) {
   // CHECK-NOT: [Error]
   // CHECK: Alloc Stats from softcounters
   // CHECK-NEXT: Total heap                 : 200 ,  200 ,    -
-  // CHECK-NEXT: Total stack                :  {{[0-9]+}} ,    0 ,    -
+  // CHECK-NEXT: Total stack                :   0 ,    0 ,    -
   // CHECK-NEXT: Total global               :   0 ,    0 ,    -
   // CHECK-NEXT: Max. Heap Allocs           :   2 ,    - ,    -
-  // CHECK-NEXT: Max. Stack Allocs          :  {{[0-9]+}} ,    - ,    -
+  // CHECK-NEXT: Max. Stack Allocs          :   0 ,    - ,    -
   // CHECK-NEXT: Addresses checked          :   0 ,    - ,    -
   // CHECK-NEXT: Distinct Addresses checked :   0 ,    - ,    -
   // CHECK-NEXT: Addresses re-used          :   0 ,    - ,    -
   // CHECK-NEXT: Addresses missed           :   0 ,    - ,    -
   // CHECK-NEXT: Distinct Addresses missed  :   0 ,    - ,    -
   // CHECK-NEXT: Total free heap            : 200 ,  200 ,    -
-  // CHECK-NEXT: Total free stack           :  {{[0-9]+}} ,    0 ,    -
-  // CHECK-NEXT: OMP Stack/Heap/Free        :  {{[0-9]+}} ,  200 ,  200
+  // CHECK-NEXT: Total free stack           :   0 ,    0 ,    -
+  // CHECK-NEXT: OMP Stack/Heap/Free        :   0 ,  200 ,  200
   // CHECK-NEXT: Null/Zero/NullZero Addr    :   0 ,    0 ,    0
   // CHECK-NEXT: User-def. types            :   0 ,    - ,    -
   // CHECK-NEXT: Estimated memory use (KiB) :   {{[0-9]+}} ,    - ,    -
@@ -50,7 +51,7 @@ int main(int argc, char** argv) {
   // CHECK-NEXT: Allocation type detail (heap, stack, global)
   // CHECK: {{(#|-)+}}
   // CHECK-NEXT: Free allocation type detail (heap, stack)
-  // CHECK: 6 : 200 ,    0 , double
+  // CHECK-NEXT: 6 : 200 ,    0 , double
 
 
   return 0;
