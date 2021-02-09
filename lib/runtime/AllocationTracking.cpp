4e6c4e5ef4d1759ed773a310e8c1e385142c0f89
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2021-02-09 14:05:17 +0100

    Minor changes to runtime guard

--- lib/runtime/AllocationTracking.cpp
+++ lib/runtime/AllocationTracking.cpp
@@ -21,9 +21,13 @@ using namespace btree;
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
-#define TYPEART_RUNTIME_GUARD(NAME) \
-  typeart::RTGuardRAII NAME;        \
-  if (!NAME.shouldTrack())          \
+#define CONCAT_(x,y) x##y
+#define CONCAT(x,y) CONCAT_(x,y)
+#define GUARDNAME CONCAT(typeart_guard_, __LINE__)
+
+#define TYPEART_RUNTIME_GUARD \
+  typeart::RTGuard GUARDNAME;        \
+  if (!GUARDNAME.shouldTrack())          \
     return;
 
 namespace typeart {
@@ -258,58 +262,58 @@ llvm::Optional<RuntimeT::MapEntry> AllocationTracker::findBaseAlloc(const void* 
 }  // namespace typeart
 
 void __typeart_alloc(const void* addr, int typeId, size_t count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onAlloc(addr, typeId, count, retAddr);
 }
 
 void __typeart_alloc_stack(const void* addr, int typeId, size_t count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onAllocStack(addr, typeId, count, retAddr);
 }
 
 void __typeart_alloc_global(const void* addr, int typeId, size_t count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onAllocGlobal(addr, typeId, count, retAddr);
 }
 
 void __typeart_free(const void* addr) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onFreeHeap(addr, retAddr);
 }
 
 void __typeart_leave_scope(int alloca_count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onLeaveScope(alloca_count, retAddr);
 }
 
 void __typeart_alloc_omp(const void* addr, int typeId, size_t count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onAlloc(addr, typeId, count, retAddr);
   typeart::RuntimeSystem::get().recorder.incOmpContextHeap();
 }
 
 void __typeart_alloc_stack_omp(const void* addr, int typeId, size_t count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onAllocStack(addr, typeId, count, retAddr);
   typeart::RuntimeSystem::get().recorder.incOmpContextStack();
 }
 
 void __typeart_free_omp(const void* addr) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onFreeHeap(addr, retAddr);
   typeart::RuntimeSystem::get().recorder.incOmpContextFree();
 }
 
 void __typeart_leave_scope_omp(int alloca_count) {
-  TYPEART_RUNTIME_GUARD(guard);
+  TYPEART_RUNTIME_GUARD;
   const void* retAddr = __builtin_return_address(0);
   typeart::RuntimeSystem::get().allocTracker.onLeaveScope(alloca_count, retAddr);
 }
--- lib/runtime/Runtime.h
+++ lib/runtime/Runtime.h
@@ -56,12 +56,12 @@ struct RuntimeSystem {
   ~RuntimeSystem();
 };
 
-struct RTGuardRAII final {
-  RTGuardRAII() : alreadyInRT(typeart::RuntimeSystem::rtScope) {
+struct RTGuard final {
+  RTGuard() : alreadyInRT(typeart::RuntimeSystem::rtScope) {
     typeart::RuntimeSystem::rtScope = true;
   }
 
-  ~RTGuardRAII() {
+  ~RTGuard() {
     if (!alreadyInRT)
       typeart::RuntimeSystem::rtScope = false;
   }
