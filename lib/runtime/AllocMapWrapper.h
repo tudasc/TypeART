8b576c4c1922bbae55612dd35c8a7509d20472e0
Alexander Hück <hueck.alexander@gmail.com>
2021-03-26 22:16:42 +0100

    crtp -> mixin

--- lib/runtime/AllocMapWrapper.h
+++ lib/runtime/AllocMapWrapper.h
@@ -12,70 +12,9 @@
 #include <algorithm>
 
 namespace typeart {
-
 enum class Mode { thread_safe = 0, thread_unsafe };
-template <typename Derived>
-class Wrapper {
- public:
-  Derived& get() {
-    return *static_cast<Derived*>(this);
-  }
 
-  template <Mode M = Mode::thread_safe>
-  [[nodiscard]] inline llvm::Optional<RuntimeT::MappedType> find(MemAddr addr) {
-    return get().template find<M>(addr);
-  }
-
-  template <Mode m = Mode::thread_safe>
-  [[nodiscard]] inline bool put(MemAddr addr, const RuntimeT::MappedType& entry) {
-    return get().template put<m>(entry);
-  }
-
-  template <Mode m = Mode::thread_safe>
-  [[nodiscard]] inline bool remove(MemAddr addr) {
-    return get().template remove<m>(addr);
-  }
-
- protected:
-  template <typename Xlocked>
-  [[nodiscard]] inline bool do_put(Xlocked&& lockedAllocs, MemAddr addr, const RuntimeT::MappedType& data) {
-    auto& def             = (*lockedAllocs)[addr];
-    const bool overridden = (def.typeId != -1);
-    def                   = data;
-    return overridden;
-  }
-
-  template <typename Slocked>
-  [[nodiscard]] inline llvm::Optional<RuntimeT::MapEntry> do_find(Slocked&& slockedAllocs, MemAddr addr) const {
-    if (slockedAllocs->empty() || addr < slockedAllocs->begin()->first) {
-      return llvm::None;
-    }
-
-    auto it = slockedAllocs->lower_bound(addr);
-    if (it == slockedAllocs->end()) {
-      // No element bigger than base address
-      return {*slockedAllocs->rbegin()};
-    }
-
-    if (it->first == addr) {
-      // Exact match
-      return {*it};
-    }
-    // Base address
-    return {*std::prev(it)};
-  }
-
-  template <typename Xlocked>
-  [[nodiscard]] inline llvm::Optional<RuntimeT::MappedType> do_remove(Xlocked&& allocs, MemAddr addr) {
-    const auto it = allocs->find(addr);
-    if (it != allocs->end()) {
-      auto removed = it->second;
-      allocs->erase(it);
-      return removed;
-    }
-    return llvm::None;
-  }
-};
+namespace mixin {
 
 namespace detail {
 template <typename Map>
@@ -101,19 +40,60 @@ template <typename Map>
 }
 }  // namespace detail
 
-class MapWrapper : public Wrapper<MapWrapper> {
+struct MapOp {
+  template <typename Xlocked>
+  [[nodiscard]] inline static bool do_put(Xlocked&& lockedAllocs, MemAddr addr, const RuntimeT::MappedType& data) {
+    auto& def             = (*lockedAllocs)[addr];
+    const bool overridden = (def.typeId != -1);
+    def                   = data;
+    return overridden;
+  }
+
+  template <typename Slocked>
+  [[nodiscard]] inline static llvm::Optional<RuntimeT::MapEntry> do_find(Slocked&& slockedAllocs, MemAddr addr) {
+    if (slockedAllocs->empty() || addr < slockedAllocs->begin()->first) {
+      return llvm::None;
+    }
+
+    auto it = slockedAllocs->lower_bound(addr);
+    if (it == slockedAllocs->end()) {
+      // No element bigger than base address
+      return {*slockedAllocs->rbegin()};
+    }
+
+    if (it->first == addr) {
+      // Exact match
+      return {*it};
+    }
+    // Base address
+    return {*std::prev(it)};
+  }
+
+  template <typename Xlocked>
+  [[nodiscard]] inline static llvm::Optional<RuntimeT::MappedType> do_remove(Xlocked&& allocs, MemAddr addr) {
+    const auto it = allocs->find(addr);
+    if (it != allocs->end()) {
+      auto removed = it->second;
+      allocs->erase(it);
+      return removed;
+    }
+    return llvm::None;
+  }
+};
+
+template <typename BaseOp>
+struct StandardMap : public BaseOp {
   RuntimeT::PointerMapBaseT allocTypesSafe;
   mutable std::shared_mutex alloc_m;
 
- public:
   template <Mode m = Mode::thread_safe>
   [[nodiscard]] inline llvm::Optional<RuntimeT::MapEntry> find(MemAddr addr) const {
     using namespace detail;
     if constexpr (m == Mode::thread_safe) {
       std::shared_lock<std::shared_mutex> guard(alloc_m);
-      return do_find(as_ptr(allocTypesSafe), addr);
+      return BaseOp::do_find(as_ptr(allocTypesSafe), addr);
     } else {
-      return do_find(as_ptr(allocTypesSafe), addr);
+      return BaseOp::do_find(as_ptr(allocTypesSafe), addr);
     }
   }
 
@@ -122,9 +102,9 @@ class MapWrapper : public Wrapper<MapWrapper> {
     using namespace detail;
     if constexpr (m == Mode::thread_safe) {
       std::lock_guard<std::shared_mutex> guard(alloc_m);
-      return do_put(as_ptr(allocTypesSafe), addr, entry);
+      return BaseOp::do_put(as_ptr(allocTypesSafe), addr, entry);
     } else {
-      return do_put(as_ptr(allocTypesSafe), addr, entry);
+      return BaseOp::do_put(as_ptr(allocTypesSafe), addr, entry);
     }
   }
 
@@ -133,40 +113,45 @@ class MapWrapper : public Wrapper<MapWrapper> {
     using namespace detail;
     if constexpr (m == Mode::thread_safe) {
       std::lock_guard<std::shared_mutex> guard(alloc_m);
-      return do_remove(as_ptr(allocTypesSafe), addr);
+      return BaseOp::do_remove(as_ptr(allocTypesSafe), addr);
     } else {
-      return do_remove(as_ptr(allocTypesSafe), addr);
+      return BaseOp::do_remove(as_ptr(allocTypesSafe), addr);
     }
   }
 };
 
 #ifdef USE_SAFEPTR
-class SafePtrWrapper : public Wrapper<SafePtrWrapper> {
+template <typename BaseOp>
+struct SafePtrdMap : public BaseOp {
   RuntimeT::PointerMap allocTypesSafe;
 
- public:
   template <Mode m = Mode::thread_safe>
   [[nodiscard]] inline llvm::Optional<RuntimeT::MapEntry> find(MemAddr addr) const {
+    static_assert(m != Mode::thread_unsafe, "SafePtrMap is always thread safe.");
     auto slockedAllocs = sf::slock_safe_ptr(allocTypesSafe);
-    return do_find(slockedAllocs, addr);
+    return BaseOp::do_find(slockedAllocs, addr);
   }
   template <Mode m = Mode::thread_safe>
   [[nodiscard]] inline bool put(MemAddr addr, const RuntimeT::MappedType& entry) {
+    static_assert(m != Mode::thread_unsafe, "SafePtrMap is always thread safe.");
     auto guard = sf::xlock_safe_ptr(allocTypesSafe);
-    return do_put(guard, addr, entry);
+    return BaseOp::do_put(guard, addr, entry);
   }
   template <Mode m = Mode::thread_safe>
   [[nodiscard]] inline llvm::Optional<RuntimeT::MappedType> remove(MemAddr addr) {
+    static_assert(m != Mode::thread_unsafe, "SafePtrMap is always thread safe.");
     auto guard = sf::xlock_safe_ptr(allocTypesSafe);
-    return do_remove(guard, addr);
+    return BaseOp::do_remove(guard, addr);
   }
 };
 #endif
+}  // namespace mixin
 
 #ifdef USE_SAFEPTR
-using PointerMap = SafePtrWrapper;
+using PointerMap = mixin::SafePtrdMap<mixin::MapOp>;
 #else
-using PointerMap = MapWrapper;
+using PointerMap = mixin::StandardMap<mixin::MapOp>;
+;
 #endif
 
 }  // namespace typeart
