a3c27c49d8af22e4125c7cdb406d8aad211ccec5
Alexander Hück <hueck.alexander@gmail.com>
2021-04-08 23:19:21 +0200

    test and fix for #67

---
+++ test/runtime/40_ret_addr.c
@@ -0,0 +1,22 @@
+// RUN: %run %s --manual 2>&1 | FileCheck %s
+
+#include "../../lib/runtime/CallbackInterface.h"
+#include "util.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+  const void* ret_check=NULL;
+  const void* addr = 1;
+
+  typeart_get_return_address(addr, &ret_check);
+  if(ret_check != NULL){
+    fprintf(stderr,"[Error] Ret address mismatch expected NULL but have %p\n", ret_check);
+  }
+  return 0;
+}
+
+// CHECK-NOT: [Error]
+
