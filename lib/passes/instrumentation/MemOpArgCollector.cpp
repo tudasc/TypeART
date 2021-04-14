f29991ea8bcd6ec298d691e8301209e6997dbb27
Alexander Hück <hueck.alexander@gmail.com>
2021-04-14 14:22:30 +0200

    unknown types not instrumented
    - This handles undefined (TypeART) behavior for unknown types, see e.g., #62
    - Libc++-based build with lit-test-suite works now, see #60

--- lib/passes/instrumentation/MemOpArgCollector.cpp
+++ lib/passes/instrumentation/MemOpArgCollector.cpp
@@ -50,7 +50,7 @@ HeapArgList MemOpArgCollector::collectHeap(const MallocDataList& mallocs) {
     int typeId     = type_m->getOrRegisterType(malloc_call->getType()->getPointerElementType(),
                                            dl);  // retrieveTypeID(tu::getVoidType(c));
     if (typeId == TA_UNKNOWN_TYPE) {
-      LOG_ERROR("Unknown allocated type. Not instrumenting. " << util::dump(*malloc_call));
+      LOG_ERROR("Unknown heap type. Not instrumenting. " << util::dump(*malloc_call));
       // TODO notify caller that we skipped: via lambda callback function
       continue;
     }
@@ -79,7 +79,7 @@ HeapArgList MemOpArgCollector::collectHeap(const MallocDataList& mallocs) {
         continue;
       }
     } else {
-      LOG_ERROR("Primary bitcast is null. malloc: " << util::dump(*malloc_call))
+      LOG_WARNING("Primary bitcast is null. malloc: " << util::dump(*malloc_call))
     }
 
     auto* typeIdConst    = instr_helper->getConstantFor(IType::type_id, typeId);
@@ -183,7 +183,8 @@ StackArgList MemOpArgCollector::collectStack(const AllocaDataList& allocs) {
     int typeId = type_m->getOrRegisterType(elementType, dl);
 
     if (typeId == TA_UNKNOWN_TYPE) {
-      LOG_ERROR("Type is not supported: " << util::dump(*elementType));
+      LOG_ERROR("Unknown stack type. Not instrumenting. " << util::dump(*elementType));
+      continue;
     }
 
     auto* typeIdConst = instr_helper->getConstantFor(IType::type_id, typeId);
@@ -214,7 +215,13 @@ GlobalArgList MemOpArgCollector::collectGlobal(const GlobalDataList& globals) {
       type        = tu::getArrayElementType(type);
     }
 
-    int typeId             = type_m->getOrRegisterType(type, dl);
+    int typeId = type_m->getOrRegisterType(type, dl);
+
+    if (typeId == TA_UNKNOWN_TYPE) {
+      LOG_ERROR("Unknown global type. Not instrumenting. " << util::dump(*type));
+      continue;
+    }
+
     auto* typeIdConst      = instr_helper->getConstantFor(IType::type_id, typeId);
     auto* numElementsConst = instr_helper->getConstantFor(IType::extent, numElements);
     // auto globalPtr         = IRB.CreateBitOrPointerCast(global, instr.getTypeFor(IType::ptr));
