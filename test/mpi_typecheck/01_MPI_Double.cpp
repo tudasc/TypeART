1fb3c75a0b57796080625596ae91b5d76cffa44f
Alexander Linne <alexander.linne@outlook.com>
2021-06-16 18:37:31 +0200

    add ability to redirect logs to files for MPI tests
    As MPI tests are run in multiple processes, the output of a call
    to mpirun cannot be directly piped to FileCheck. Instead, instruct
    mpirun to write the output to files and use cat to pipe the contents
    of all files to FileCheck.

--- test/mpi_typecheck/01_MPI_Double.cpp
+++ test/mpi_typecheck/01_MPI_Double.cpp
@@ -1,4 +1,4 @@
-// RUN: %run %s --mpi_intercept 2>&1 | FileCheck %s
+// RUN: %run %s --mpi_intercept --mpi_output_filename "%s.log" 2>&1 && find . -type f -exec cat {} \; | FileCheck %s
 
 #include <mpi.h>
 #include <stdlib.h>
@@ -16,8 +16,10 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if (rank == 0) {
+    // CHECK: R[0][Info][1] MPI_Send: buffer 0x{{.*}} has type double, MPI type is double
     MPI_Send(f, n, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
   } else {
+    // CHECK: R[1][Info][0] MPI_Recv: buffer 0x{{.*}} has type double, MPI type is double
     MPI_Recv(f, n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
 
