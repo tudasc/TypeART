054ffd62b31d7711519b64770126386b0e6effff
Alexander Hück <alexander.hueck@sc.tu-darmstadt.de>
2021-07-22 13:21:36 +0200

    CMake build system modernization (#86)
    Changes aim to modernize the CMake build files and provide a more standardized install structure for TypeART. To that end, the following main changes were made:
    - Unified script templates (further)
    - Renamed libraries
    - Install file prefix for all is (lib)"typeart"[...]
    - Install folders (prepent *prefix*): bin, include/typeart, lib, lib/cmake/typeart
    - Proper CMake packaging with component support
    - CMake presets provided, used for CI runs
    - Relocatibility: Make use of generator expressions for targets etc. to abstract away from hard-coded (lib) names in CMake files
    - Provides CMake feature summary after configure step

--- cmake/modules/coverage-lcov.cmake
+++ cmake/modules/coverage-lcov.cmake
@@ -22,7 +22,8 @@ endif()
 
 add_custom_target(
   lcov-make
-  COMMAND ${LCOV_COMMAND} ${GCOV_TOOL} ${GCOV_WORKAROUND} --no-external -c -d ${CMAKE_BINARY_DIR} -b ${CMAKE_SOURCE_DIR} -o typeart.coverage
+  COMMAND ${LCOV_COMMAND} ${GCOV_TOOL} ${GCOV_WORKAROUND}
+          --no-external -c -d ${CMAKE_BINARY_DIR} -b ${CMAKE_SOURCE_DIR} -o typeart.coverage
   COMMAND ${LCOV_COMMAND} --remove typeart.coverage '${CMAKE_BINARY_DIR}/*' -o typeart.coverage
 )
 
@@ -31,7 +32,6 @@ add_custom_target(
   COMMAND ${GENHTML_COMMAND} -o ${TYPEART_PROFILE_DIR} typeart.coverage
   DEPENDS lcov-make
 )
-
 
 function(make_lcov_target target)
 #  add_custom_target(
@@ -44,8 +44,11 @@ function(make_lcov_target target)
 
   add_custom_target(
     lcov-make-${target}
-    COMMAND ${LCOV_COMMAND} ${GCOV_TOOL} ${GCOV_WORKAROUND} --no-external -c -d ${CMAKE_BINARY_DIR} -b ${LCOV_TARGET_SOURCE_DIR} -o counter-${target}.pro
-    COMMAND ${LCOV_COMMAND} --remove counter-${target}.pro '${CMAKE_BINARY_DIR}/*' -o counter-${target}.pro
+    COMMAND ${LCOV_COMMAND} ${GCOV_TOOL} ${GCOV_WORKAROUND}
+            --no-external -c -d ${CMAKE_BINARY_DIR}
+            -b ${LCOV_TARGET_SOURCE_DIR} -o counter-${target}.pro
+    COMMAND ${LCOV_COMMAND} --remove counter-${target}.pro '${CMAKE_BINARY_DIR}/*'
+            -o counter-${target}.pro
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
 
--- cmake/modules/llvm-util.cmake
+++ cmake/modules/llvm-util.cmake
@@ -1,7 +1,7 @@
 function(make_llvm_module name sources)
   # TODO default of include_dirs is private
   cmake_parse_arguments(ARG "" "" "INCLUDE_DIRS;DEPENDS;LINK_LIBS" ${ARGN})
-  
+
   # TODO find out which LLVM versions support the respective calls
   if(${LLVM_PACKAGE_VERSION} VERSION_GREATER_EQUAL "10.0.0")
     add_llvm_pass_plugin(${name}
@@ -24,23 +24,22 @@ function(make_llvm_module name sources)
         LLVM_VERSION=6
     )
   endif()
-  
-  
+
   target_include_directories(${name}
-    SYSTEM 
-    PUBLIC 
+    SYSTEM
+    PRIVATE
     ${LLVM_INCLUDE_DIRS}
   )
-  
+
   if(ARG_INCLUDE_DIRS)
     target_include_directories(${name}
       PRIVATE
       ${ARG_INCLUDE_DIRS}
     )
   endif()
-  
+
   target_define_file_basename(${name})
-  
+
   target_compile_definitions(${name}
     PRIVATE
     ${LLVM_DEFINITIONS}
