4eeb0c926c267c1aa84627ec84cd5f2a4d06e1e9
Alexander Linne <alexander.linne@outlook.com>
2021-08-01 16:40:04 +0200

    implement type check for MPI_Type_create_struct

--- lib/mpi_interceptor/TypeCheck.h
+++ lib/mpi_interceptor/TypeCheck.h
@@ -78,6 +78,47 @@ int ta_check_type(const MPICallInfo* call, const MPIBufferInfo* buffer, const MP
       *mpi_count *= max_displacement + array_of_integers[1];
       return result;
     }
+    case MPI_COMBINER_STRUCT: {
+      typeart_struct_layout struct_layout;
+      typeart_status status = typeart_resolve_type(buffer->type_id, &struct_layout);
+      if (status != TA_OK) {
+        PRINT_ERRORV(call, "expected a struct type, but found type \"%s\"\n", buffer->type_name);
+        return -1;
+      }
+      if (struct_layout.len != array_of_integers[0]) {
+        PRINT_ERRORV(call, "expected %d members, but the type \"%s\" has %ld members\n", array_of_integers[0],
+                     buffer->type_name, struct_layout.len);
+        return -1;
+      }
+      int result = 0;
+      for (size_t i = 0; i < struct_layout.len; ++i) {
+        if (struct_layout.offsets[i] != array_of_addresses[i]) {
+          PRINT_ERRORV(call, "expected a byte offset of %ld for member %ld, but the type \"%s\" has an offset of %ld\n",
+                       array_of_addresses[i], i + 1, buffer->type_name, struct_layout.offsets[i]);
+          result = -1;
+        }
+      }
+      for (size_t i = 0; i < struct_layout.len; ++i) {
+        const void* member_ptr      = buffer->ptr + struct_layout.offsets[i];
+        int member_type_id          = struct_layout.member_types[i];
+        MPIBufferInfo member_buffer = {member_ptr, struct_layout.count[i], member_type_id,
+                                       typeart_get_type_name(member_type_id)};
+        MPITypeInfo member_type;
+        ta_create_type_info(call, array_of_datatypes[i], &member_type);
+        int member_element_count;
+        if (ta_check_type(call, &member_buffer, &member_type, &member_element_count) != 0) {
+          result = -1;
+          PRINT_ERRORV(call, "the typechek for member %ld failed\n", i + 1);
+        }
+        if (member_buffer.count * member_element_count != array_of_integers[i + 1]) {
+          result = -1;
+          PRINT_ERRORV(call, "expected element count of %d for member %ld, but the type \"%s\" has a count of %d\n",
+                       array_of_integers[i + 1], i + 1, buffer->type_name, member_buffer.count * member_element_count);
+        }
+      }
+      *mpi_count = 1;
+      return result;
+    }
     default:
       PRINT_ERRORV(call, "the MPI type combiner %s is currently not supported\n", ta_mpi_combiner_name(combiner));
   }
---
+++ test/mpi_typecheck/06_MPI_Type_struct.cpp
@@ -0,0 +1,99 @@
+// clang-format off
+// RUN: %run %s --mpi_intercept --mpi_output_filename "%s.log" && cat "%s.log/1/rank.0/stderr" | FileCheck --check-prefixes CHECK,RANK0 %s && cat "%s.log/1/rank.1/stderr" | FileCheck --check-prefixes CHECK,RANK1 %s
+// clang-format on
+
+#include <array>
+#include <mpi.h>
+#include <stdlib.h>
+
+struct S1 {
+  double a[2];
+  int b;
+  double c;
+};
+
+void run_test(void* data, int count, int integers[], MPI_Aint addrs[], MPI_Datatype types[]) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Datatype datatype;
+  MPI_Type_create_struct(count, integers, addrs, types, &datatype);
+  MPI_Type_set_name(datatype, "test_type");
+  MPI_Type_commit(&datatype);
+  if (rank == 0) {
+    MPI_Send(data, 1, datatype, 1, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Recv(data, 1, datatype, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+  MPI_Type_free(&datatype);
+}
+
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  // CHECK: [Trace] TypeART Runtime Trace
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  int counts[3]         = {2, 1, 1};
+  MPI_Aint offsets[3]   = {offsetof(S1, a), offsetof(S1, c), offsetof(S1, c)};
+  MPI_Datatype types[3] = {MPI_DOUBLE, MPI_INT, MPI_DOUBLE};
+
+  double arr[3];
+  S1 s1;
+
+  // 1: Check non-struct buffer type and wrong member count
+  // clang-format off
+  // RANK0: R[0][Info]ID[0] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK0: R[0][Error]ID[0] expected a struct type, but found type "double"
+  // RANK1: R[1][Info]ID[0] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Error]ID[0] expected a struct type, but found type "double"
+  // clang-format on
+  run_test(arr, 2, counts, offsets, types);
+
+  // clang-format off
+  // RANK0: R[0][Info]ID[1] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK0: R[0][Error]ID[1] expected 2 members, but the type "struct.S1" has 3 members
+  // RANK1: R[1][Info]ID[1] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Error]ID[1] expected 2 members, but the type "struct.S1" has 3 members
+  // clang-format on
+  run_test(&s1, 2, counts, offsets, types);
+
+  // 2: Check wrong offsets
+  // clang-format off
+  // RANK0: R[0][Info]ID[2] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK0: R[0][Error]ID[2] expected a byte offset of 24 for member 2, but the type "struct.S1" has an offset of 16
+  // RANK1: R[1][Info]ID[2] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Error]ID[2] expected a byte offset of 24 for member 2, but the type "struct.S1" has an offset of 16
+  // clang-format on
+  run_test(&s1, 3, counts, (MPI_Aint[3]){offsetof(S1, a), offsetof(S1, c), offsetof(S1, c)}, types);
+
+  // 3: Check wrong types
+  // clang-format off
+  // RANK0: R[0][Info]ID[3] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK0: R[0][Error]ID[3] expected a type matching MPI type "MPI_INT", but found type "double"
+  // RANK0: R[0][Error]ID[3] the typechek for member 3 failed
+  // RANK1: R[1][Info]ID[3] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Error]ID[3] expected a type matching MPI type "MPI_INT", but found type "double"
+  // RANK1: R[1][Error]ID[3] the typechek for member 3 failed
+  // clang-format on
+  run_test(&s1, 3, counts, offsets, (MPI_Datatype[3]){MPI_DOUBLE, MPI_INT, MPI_INT});
+
+  // 3: Check member count
+  // clang-format off
+  // RANK0: R[0][Info]ID[4] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK0: R[0][Error]ID[4] expected element count of 1 for member 1, but the type "struct.S1" has a count of 2
+  // RANK1: R[1][Info]ID[4] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Error]ID[4] expected element count of 1 for member 1, but the type "struct.S1" has a count of 2
+  // clang-format on
+  run_test(&s1, 3, (int[3]){1, 1, 1}, offsets, types);
+
+  // 3: Check member count
+  // clang-format off
+  // RANK0: R[0][Info]ID[5] MPI_Send at 0x{{.*}} in function _Z8run_test{{.*}}: checking send-buffer 0x{{.*}} against MPI type "test_type"
+  // RANK1: R[1][Info]ID[5] MPI_Recv at 0x{{.*}} in function _Z8run_test{{.*}}: checking recv-buffer 0x{{.*}} against MPI type "test_type"
+  // clang-format on
+  run_test(&s1, 3, counts, offsets, types);
+
+  MPI_Finalize();
+  return 0;
+}
