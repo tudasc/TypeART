fe8d707852bc26e7e064cecd471f0ab767b58d24
Alexander Hück <hueck.alexander@gmail.com>
2021-12-23 12:46:42 +0100

    use substitution for opt

--- test/pass/filter/07_lulesh_mock.cpp
+++ test/pass/filter/07_lulesh_mock.cpp
@@ -1,8 +1,8 @@
 // clang-format off
 // RUN: %c-to-llvm -fno-discard-value-names %s | %apply-typeart -typeart-alloca -call-filter -call-filter-impl=deprecated::default -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s
-// RUN: %c-to-llvm -fno-discard-value-names %s | opt -O3 -S | %apply-typeart -typeart-alloca -call-filter -call-filter-impl=deprecated::default -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=CHECK-opt
+// RUN: %c-to-llvm -fno-discard-value-names %s | %opt -O3 -S | %apply-typeart -typeart-alloca -call-filter -call-filter-impl=deprecated::default -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=CHECK-opt
 // RUN: %c-to-llvm -fno-discard-value-names %s | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=CHECK-exp-default
-// RUN: %c-to-llvm -fno-discard-value-names %s | opt -O3 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=CHECK-exp-default-opt
+// RUN: %c-to-llvm -fno-discard-value-names %s | %opt -O3 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=CHECK-exp-default-opt
 // clang-format on
 
 using Real_t = double;
--- test/pass/filter/20_omp_priv_combi_nested.c
+++ test/pass/filter/20_omp_priv_combi_nested.c
@@ -1,9 +1,9 @@
 // clang-format off
 // RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s
-// RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | opt -O2 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=check-opt
+// RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | %opt -O2 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S 2>&1 | %filecheck %s --check-prefix=check-opt
 
 // RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S | %filecheck %s --check-prefix=check-inst
-// RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | opt -O2 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S | %filecheck %s --check-prefix=check-opt-inst
+// RUN: %c-to-llvm -fno-discard-value-names %omp_c_flags %s | %opt -O2 -S | %apply-typeart -typeart-alloca -call-filter -typeart-filter-pointer-alloca=false -S | %filecheck %s --check-prefix=check-opt-inst
 // REQUIRES: openmp
 // clang-format on
 
