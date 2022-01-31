2100e70ad66669664cc72786712ad45772214d5d
Alexander Hück <hueck.alexander@gmail.com>
2022-01-31 16:45:21 +0100

    Fix wrapper for single TU (#111)
    * Fix handling of compile options primarily w.r.t. linking detection
    * Add compile + link in single step to produce executable

--- test/script/04_wrapper_cc.c
+++ test/script/04_wrapper_cc.c
@@ -2,6 +2,10 @@
 // RUN: %wrapper-cc -O1 %s -o %s.exe
 // RUN: %s.exe 2>&1 | %filecheck %s
 
+// RUN: %wrapper-cc -O1 -c %s -o %s.o
+// RUN: %wrapper-cc %s.o -o %s.exe
+// RUN: %mpi-exec -np 1 %s.exe 2>&1 | %filecheck %s
+
 #include "../../lib/runtime/CallbackInterface.h"
 
 int main(int argc, char** argv) {
