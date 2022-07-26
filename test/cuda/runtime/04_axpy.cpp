93e35adff8f5331b6fcd6d3aacd980b2845fb7a1
Alexander Hück <hueck.alexander@gmail.com>
2022-07-26 17:44:19 +0200

    support llvm cuda style compiler flags

---
+++ test/cuda/runtime/04_axpy.cpp
@@ -0,0 +1,64 @@
+// RUN: %wrapper-cxx -x cuda --cuda-gpu-arch=sm_72 %s -o %s.exe
+// RUN: %s.exe 2>&1 | %filecheck %s
+
+// REQUIRES: cuda && softcounter
+
+// CHECK: Status OK
+// CHECK: Status OK
+// CHECK: Status OK
+// CHECK: Status OK
+// CHECK: 5   :   2 ,    4 ,    1 , float
+
+#include "../../lib/runtime/RuntimeInterface.h"
+
+#include <iostream>
+
+void type_check(const void* addr) {
+  int id_result      = 0;
+  size_t count_check = 0;
+  typeart_status status;
+  status = typeart_get_type(addr, &id_result, &count_check);
+
+  if (status != TYPEART_OK) {
+    std::cerr << "[Error]: Status not OK: " << status << " " << addr << "\n";
+  } else {
+    std::cerr << "Status OK: " << id_result << " " << count_check << "\n";
+  }
+}
+
+__global__ void axpy(float a, float* x, float* y) {
+  y[threadIdx.x] = a * x[threadIdx.x];
+}
+
+int main(int argc, char* argv[]) {
+  const int kDataLen = 4;
+
+  float a                = 2.0f;
+  float host_x[kDataLen] = {1.0f, 2.0f, 3.0f, 4.0f};
+  float host_y[kDataLen];
+
+  type_check((void*)&host_x[0]);
+  type_check((void*)&host_y[0]);
+
+  float* device_x;
+  float* device_y;
+  cudaMalloc((void**)&device_x, kDataLen * sizeof(float));
+  cudaMalloc((void**)&device_y, kDataLen * sizeof(float));
+
+  type_check((void*)device_x);
+  type_check((void*)device_y);
+
+  cudaMemcpy(device_x, host_x, kDataLen * sizeof(float), cudaMemcpyHostToDevice);
+
+  axpy<<<1, kDataLen>>>(a, device_x, device_y);
+
+  cudaDeviceSynchronize();
+  cudaMemcpy(host_y, device_y, kDataLen * sizeof(float), cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < kDataLen; ++i) {
+    std::cout << "y[" << i << "] = " << host_y[i] << "\n";
+  }
+
+  cudaDeviceReset();
+  return 0;
+}
