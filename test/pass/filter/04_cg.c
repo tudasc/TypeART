2bef3e5af6ee3548797a5545a7ee4854dfda88bd
Lambeth, Steven <819106+ste-lam@users.noreply.github.com>
2023-02-22 12:47:52 +0100

    extended existing cg filter tests for new acg filter

--- test/pass/filter/04_cg.c
+++ test/pass/filter/04_cg.c
@@ -1,9 +1,10 @@
 // clang-format off
+// RUN: %c-to-llvm %s | %apply-typeart --typeart-stack --typeart-filter --typeart-filter-implementation=acg --typeart-filter-cg-file=%p/04_cg.ipcg2 -S 2>&1 | %filecheck %s
 // RUN: %c-to-llvm %s | %apply-typeart --typeart-stack --typeart-filter --typeart-filter-implementation=cg --typeart-filter-cg-file=%p/04_cg.ipcg -S 2>&1 | %filecheck %s
 // RUN: %c-to-llvm %s | %apply-typeart --typeart-stack --typeart-filter -S 2>&1 | %filecheck %s --check-prefix=CHECK-default
 // clang-format on
 
-extern void bar(int* ptr);  // reaches MPI, see 04_cg.ipcg
+extern void bar(int* ptr);  // reaches MPI, see 04_cg.ipcg / 04_cg.ipcg2
 extern void aar(int* ptr);  // does not reach MPI
 
 void foo() {
@@ -11,7 +12,7 @@ void foo() {
   bar(&a);
   aar(&b);
 }
-// CG:
+// CG / ACG:
 // CHECK: > Stack Memory
 // CHECK-NEXT: Alloca                 :  2.00
 // CHECK-NEXT: Stack call filtered %  : 50.00
