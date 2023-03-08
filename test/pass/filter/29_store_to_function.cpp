9da0ece8a9358f88db35c6f0d99d66d64aab0fda
Lambeth, Steven <819106+ste-lam@users.noreply.github.com>
2023-03-08 13:00:00 +0100

    acg-test for callsites without preceding arguments

---
+++ test/pass/filter/29_store_to_function.cpp
@@ -0,0 +1,17 @@
+// clang-format off
+// RUN: %cpp-to-llvm -Xclang -disable-lifetime-markers %s | %opt -mem2reg -S | %apply-typeart --typeart-stack --typeart-filter --typeart-filter-implementation=acg --typeart-filter-cg-file=%p/29_cg.ipcg2 -S 2>&1 | %filecheck %s
+// clang-format on
+
+#include <vector>
+
+
+int& ref(int&) noexcept;
+
+void foo(int r) {
+  int i = r * 789;
+  ref(r) += i;
+}
+
+// CHECK: > Stack Memory
+// CHECK-NEXT: Alloca                      :   1.00
+// CHECK-NEXT: Stack call filtered %       : 100.00
