850f2a9d706d88ecba121ca5ea85bd4f63c265b0
Alexander Hück <hueck.alexander@gmail.com>
2024-12-18 13:57:18 +0100

    Fixes for default -g flag

--- test/script/01_wrapper_internal.sh
+++ test/script/01_wrapper_internal.sh
@@ -44,7 +44,7 @@ typeart_is_linking_fn -c
 echo $?
 
 # CHECK: 1
-typeart_skip_compile_fn -E main.c
+typeart_skip_fn -E main.c
 echo $?
 
 function typeart_lit_parse_check_fn() {
@@ -74,36 +74,36 @@ typeart_lit_parse_check_fn
 
 # CHECK: 1 1 0 0 0 0 -O3
 # CHECK-NEXT: lulesh.cc lulesh.o ea eb
-# CHECK-NEXT: -DUSE_MPI=1 -g -I. -Wall
-typeart_parse_cmd_line_fn -DUSE_MPI=1 -g -I. -Wall -O3 -c -o lulesh.o lulesh.cc
+# CHECK-NEXT: -DUSE_MPI=1 -I. -Wall
+typeart_parse_cmd_line_fn -DUSE_MPI=1 -I. -Wall -O3 -c -o lulesh.o lulesh.cc
 typeart_lit_parse_check_fn
 echo "${typeart_wrapper_more_args}"
 
 # a linker call:
 # CHECK: 0 0 0 0 0 0 -O3
 # CHECK-NEXT: es eo ea eb
-# CHECK-NEXT: -DUSE_MPI=1 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -g -lm lulesh2.0
-typeart_parse_cmd_line_fn -DUSE_MPI=1 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -g -O3 -lm -o lulesh2.0
+# CHECK-NEXT: -DUSE_MPI=1 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -lm lulesh2.0
+typeart_parse_cmd_line_fn -DUSE_MPI=1 lulesh.o lulesh-comm.o lulesh-viz.o lulesh-util.o lulesh-init.o -O3 -lm -o lulesh2.0
 typeart_lit_parse_check_fn
 echo "${typeart_wrapper_more_args}"
 
 # CHECK: 1 1 0 0 0 0 -O2
 # CHECK-NEXT: io_nonansi.c io_nonansi.o ea eb
-# CHECK-NEXT: -I. -DFN -DFAST -DCONGRAD_TMP_VECTORS -DDSLASH_TMP_LINKS -g
+# CHECK-NEXT: -I. -DFN -DFAST -DCONGRAD_TMP_VECTORS -DDSLASH_TMP_LINKS
 typeart_parse_cmd_line_fn -c -I. -DFN -DFAST -DCONGRAD_TMP_VECTORS -DDSLASH_TMP_LINKS -g -O2 io_nonansi.c -o io_nonansi.o
 typeart_lit_parse_check_fn
 echo "${typeart_wrapper_more_args}"
 
 # CHECK: 1 1 0 0 0 0 -O2
 # CHECK-NEXT: mgfparse.c mgfparse.o ea eb
-# CHECK-NEXT: -DSPEC_MPI -DNDEBUG -g
+# CHECK-NEXT: -DSPEC_MPI -DNDEBUG
 typeart_parse_cmd_line_fn -DSPEC_MPI -DNDEBUG -g -O2 -c mgfparse.c -o mgfparse.o
 typeart_lit_parse_check_fn
 echo "${typeart_wrapper_more_args}"
 
 # CHECK: 1 1 0 0 0 0 -O2
 # CHECK-NEXT: amg2013.c amg2013.o ea eb
-# CHECK-NEXT: -I.. -I../utilities -I../struct_mv -I../sstruct_mv -I../IJ_mv -I../seq_mv -I../parcsr_mv -I../parcsr_ls -I../krylov -DHYPRE_USING_OPENMP -DTIMER_USE_MPI -DHYPRE_LONG_LONG -DHYPRE_NO_GLOBAL_PARTITION -g -fopenmp -DHYPRE_TIMING
+# CHECK-NEXT: -I.. -I../utilities -I../struct_mv -I../sstruct_mv -I../IJ_mv -I../seq_mv -I../parcsr_mv -I../parcsr_ls -I../krylov -DHYPRE_USING_OPENMP -DTIMER_USE_MPI -DHYPRE_LONG_LONG -DHYPRE_NO_GLOBAL_PARTITION -fopenmp -DHYPRE_TIMING
 typeart_parse_cmd_line_fn -o amg2013.o -c -I.. -I../utilities -I../struct_mv -I../sstruct_mv -I../IJ_mv -I../seq_mv -I../parcsr_mv -I../parcsr_ls -I../krylov -DHYPRE_USING_OPENMP -DTIMER_USE_MPI -DHYPRE_LONG_LONG -DHYPRE_NO_GLOBAL_PARTITION -O2 -g -fopenmp -DHYPRE_TIMING amg2013.c
 typeart_lit_parse_check_fn
 echo "${typeart_wrapper_more_args}"
