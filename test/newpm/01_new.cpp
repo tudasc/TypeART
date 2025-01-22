80c2a5c84e3beba4688b2d92be55fb088774093f
Alexander Hück <hueck.alexander@gmail.com>
2025-01-22 12:59:58 +0100

    Enable new pm for LLVM 18

---
+++ test/newpm/01_new.cpp
@@ -0,0 +1,15 @@
+// clang-format off
+// RUN: %cpp-to-llvm %s | %apply-typeart -S 2>&1 | %filecheck %s
+// clang-format on
+
+#include <new>
+// CHECK: invoke{{.*}} ptr @_Znwm(i64{{( noundef)?}} 4)
+// CHECK: call void @__typeart_alloc(ptr [[POINTER:%[0-9a-z]+]], i32 12, i64 1)
+int main() {
+  try {
+    auto s = new int;
+  } catch (...) {
+  }
+
+  return 0;
+}
