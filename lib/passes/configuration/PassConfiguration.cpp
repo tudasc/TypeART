8a106f07090af48b6b4c8a9cc645bf2939f29f24
Alexander Hück <hueck.alexander@gmail.com>
2025-01-25 09:50:41 +0100

    Complete parsing set

--- lib/passes/configuration/PassConfiguration.cpp
+++ lib/passes/configuration/PassConfiguration.cpp
@@ -4,6 +4,7 @@
 
 #include "OptionsUtil.h"
 #include "analysis/MemInstFinder.h"
+#include "configuration/Configuration.h"
 #include "support/ConfigurationBase.h"
 #include "support/ConfigurationBaseOptions.h"
 #include "support/Error.h"
@@ -27,8 +28,14 @@ struct PassStdArgsEq final {
 };
 
 llvm::Expected<TypeARTConfigOptions> parse_typeart_config(llvm::StringRef parameters) {
+  return parse_typeart_config_with_occurrence(parameters).first;
+}
+
+PassConfig parse_typeart_config_with_occurrence(llvm::StringRef parameters) {
   TypeARTConfigOptions result;
+  OptOccurrenceMap occurrence_map;
   bool global_set{false};
+
   while (!parameters.empty()) {
     llvm::StringRef parameter_name;
     std::tie(parameter_name, parameters) = parameters.split(';');
@@ -36,43 +43,85 @@ llvm::Expected<TypeARTConfigOptions> parse_typeart_config(llvm::StringRef parame
     const bool enable = !parameter_name.consume_front("no-");
 
     if (parameter_name == ConfigStdArgs::heap) {
-      result.heap = enable;
+      result.heap                         = enable;
+      occurrence_map[ConfigStdArgs::heap] = true;
       continue;
     }
 
     if (parameter_name == ConfigStdArgs::stack) {
-      result.stack = enable;
+      result.stack                         = enable;
+      occurrence_map[ConfigStdArgs::stack] = true;
       continue;
     }
 
     if (parameter_name == ConfigStdArgs::global) {
-      global_set    = true;
-      result.global = enable;
+      global_set                            = true;
+      result.global                         = enable;
+      occurrence_map[ConfigStdArgs::global] = true;
       continue;
     }
 
     if (parameter_name == ConfigStdArgs::filter) {
-      result.filter = enable;
+      result.filter                         = enable;
+      occurrence_map[ConfigStdArgs::filter] = true;
       continue;
     }
 
     if (parameter_name == ConfigStdArgs::stats) {
-      result.statistics = enable;
+      result.statistics                    = enable;
+      occurrence_map[ConfigStdArgs::stats] = true;
       continue;
     }
 
     if (parameter_name == ConfigStdArgs::stack_lifetime) {
-      result.stack_lifetime = enable;
+      result.stack_lifetime                         = enable;
+      occurrence_map[ConfigStdArgs::stack_lifetime] = true;
       continue;
     }
 
     if (parameter_name.consume_front(PassStdArgsEq::typegen)) {
-      result.typegen = util::string_to_enum<TypegenImplementation>(parameter_name);
+      result.typegen                         = util::string_to_enum<TypegenImplementation>(parameter_name);
+      occurrence_map[ConfigStdArgs::typegen] = true;
       continue;
     }
 
     if (parameter_name.consume_front(PassStdArgsEq::filter_impl)) {
-      result.filter_config.implementation = util::string_to_enum<analysis::FilterImplementation>(parameter_name);
+      result.filter_config.implementation        = util::string_to_enum<analysis::FilterImplementation>(parameter_name);
+      occurrence_map[ConfigStdArgs::filter_impl] = true;
+      continue;
+    }
+
+    if (parameter_name.consume_front(PassStdArgsEq::filter_glob)) {
+      result.filter_config.glob                  = parameter_name;
+      occurrence_map[ConfigStdArgs::filter_glob] = true;
+      continue;
+    }
+    if (parameter_name.consume_front(PassStdArgsEq::filter_glob_deep)) {
+      result.filter_config.glob_deep                  = parameter_name;
+      occurrence_map[ConfigStdArgs::filter_glob_deep] = true;
+      continue;
+    }
+    if (parameter_name == ConfigStdArgs::analysis_filter_global) {
+      result.analysis_config.filter_global                  = enable;
+      occurrence_map[ConfigStdArgs::analysis_filter_global] = true;
+      continue;
+    }
+
+    if (parameter_name == ConfigStdArgs::analysis_filter_alloca_non_array) {
+      result.analysis_config.filter_alloca_non_array                  = enable;
+      occurrence_map[ConfigStdArgs::analysis_filter_alloca_non_array] = true;
+      continue;
+    }
+
+    if (parameter_name == ConfigStdArgs::analysis_filter_heap_alloc) {
+      result.analysis_config.filter_heap_alloc                  = enable;
+      occurrence_map[ConfigStdArgs::analysis_filter_heap_alloc] = true;
+      continue;
+    }
+
+    if (parameter_name == ConfigStdArgs::analysis_filter_pointer_alloc) {
+      result.analysis_config.filter_pointer_alloc                  = enable;
+      occurrence_map[ConfigStdArgs::analysis_filter_pointer_alloc] = true;
       continue;
     }
   }
@@ -80,7 +129,7 @@ llvm::Expected<TypeARTConfigOptions> parse_typeart_config(llvm::StringRef parame
     // Stack implies global
     result.global = result.stack;
   }
-  return result;
+  return {result, occurrence_map};
 }
 
 }  // namespace typeart::config::pass
--- lib/passes/configuration/PassConfiguration.h
+++ lib/passes/configuration/PassConfiguration.h
@@ -2,14 +2,18 @@
 #define D8AEC7EC_5687_4E81_B6E5_97074AEF6D94
 
 #include "TypeARTOptions.h"
+#include "Configuration.h"
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Error.h"
 
 namespace typeart::config::pass {
 
-llvm::Expected<TypeARTConfigOptions> parse_typeart_config(llvm::StringRef parameters);
+using PassConfig = std::pair<llvm::Expected<TypeARTConfigOptions>, OptOccurrenceMap>;
 
-}
+llvm::Expected<TypeARTConfigOptions> parse_typeart_config(llvm::StringRef parameters);
+PassConfig parse_typeart_config_with_occurrence(llvm::StringRef parameters);
+
+}  // namespace typeart::config::pass
 
 #endif /* D8AEC7EC_5687_4E81_B6E5_97074AEF6D94 */
