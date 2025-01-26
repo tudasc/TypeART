babdf7ff01beb2c32f4592a8d1d0e5e980ff7124
Alexander Hück <hueck.alexander@gmail.com>
2025-01-26 17:01:18 +0100

    Adapt folder exclusions

--- test/newpm/01_new.cpp
+++ test/newpm/01_new.cpp
@@ -2,6 +2,8 @@
 // RUN: %cpp-to-llvm %s | %apply-typeart -S 2>&1 | %filecheck %s
 // clang-format on
 
+// REQUIRES: !llvm-14
+
 #include <new>
 // CHECK: invoke{{.*}} ptr @_Znwm(i64{{( noundef)?}} 4)
 // CHECK: call void @__typeart_alloc(ptr [[POINTER:%[0-9a-z]+]], i32 12, i64 1)
