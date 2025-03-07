06d096d92334936cfaa8f8c89941ea3d8c0ea2c1
Alexander Hück <hueck.alexander@gmail.com>
2025-03-07 12:17:56 +0100

    Fix missing header

--- lib/passes/instrumentation/MemOpInstrumentation.cpp
+++ lib/passes/instrumentation/MemOpInstrumentation.cpp
@@ -35,6 +35,7 @@
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
