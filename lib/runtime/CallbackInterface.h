af3caee32cfa70d543451712983e58278df36989
Alexander Hück <hueck.alexander@gmail.com>
2025-12-19 12:33:30 +0100

    Reduce symbol visibility of runtime (#181)

--- lib/runtime/CallbackInterface.h
+++ lib/runtime/CallbackInterface.h
@@ -13,6 +13,8 @@
 #ifndef TYPEART_CALLBACKINTERFACE_H
 #define TYPEART_CALLBACKINTERFACE_H
 
+#include "RuntimeExport.h"
+
 #ifdef __cplusplus
 #include <cstddef>
 #else
@@ -23,28 +25,28 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void __typeart_alloc(const void* addr, int type_id, size_t count);
+TYPEART_EXPORT void __typeart_alloc(const void* addr, int type_id, size_t count);
 
-void __typeart_alloc_global(const void* addr, int type_id, size_t count);
-void __typeart_free(const void* addr);
+TYPEART_EXPORT void __typeart_alloc_global(const void* addr, int type_id, size_t count);
+TYPEART_EXPORT void __typeart_free(const void* addr);
 
-void __typeart_alloc_stack(const void* addr, int type_id, size_t count);
-void __typeart_leave_scope(int alloca_count);
+TYPEART_EXPORT void __typeart_alloc_stack(const void* addr, int type_id, size_t count);
+TYPEART_EXPORT void __typeart_leave_scope(int alloca_count);
 
 // Called from OpenMP context
-void __typeart_alloc_omp(const void* addr, int type_id, size_t count);
-void __typeart_free_omp(const void* addr);
-void __typeart_alloc_stack_omp(const void* addr, int type_id, size_t count);
-void __typeart_leave_scope_omp(int alloca_count);
+TYPEART_EXPORT void __typeart_alloc_omp(const void* addr, int type_id, size_t count);
+TYPEART_EXPORT void __typeart_free_omp(const void* addr);
+TYPEART_EXPORT void __typeart_alloc_stack_omp(const void* addr, int type_id, size_t count);
+TYPEART_EXPORT void __typeart_leave_scope_omp(int alloca_count);
 
 // Called for inlined type definitions mode
-void __typeart_alloc_mty(const void* addr, const void* info, size_t count);
-void __typeart_alloc_global_mty(const void* addr, const void* info, size_t count);
-void __typeart_alloc_stack_mty(const void* addr, const void* info, size_t count);
-void __typeart_register_type(const void* type);
+TYPEART_EXPORT void __typeart_alloc_mty(const void* addr, const void* info, size_t count);
+TYPEART_EXPORT void __typeart_alloc_global_mty(const void* addr, const void* info, size_t count);
+TYPEART_EXPORT void __typeart_alloc_stack_mty(const void* addr, const void* info, size_t count);
+TYPEART_EXPORT void __typeart_register_type(const void* type);
 
-void __typeart_alloc_global_mty_omp(const void* addr, const void* info, size_t count);
-void __typeart_alloc_stack_mty_omp(const void* addr, const void* info, size_t count);
+TYPEART_EXPORT void __typeart_alloc_global_mty_omp(const void* addr, const void* info, size_t count);
+TYPEART_EXPORT void __typeart_alloc_stack_mty_omp(const void* addr, const void* info, size_t count);
 #ifdef __cplusplus
 }
 #endif
