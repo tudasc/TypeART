794c66344199b318f3ebdb61d05b57777535a0bb
Alexander Hück <hueck.alexander@gmail.com>
2026-02-17 13:57:31 +0100

    Try to find not

--- test/pass/filter/26_malformed_json.c
+++ test/pass/filter/26_malformed_json.c
@@ -2,6 +2,8 @@
 // RUN: %c-to-llvm %s | %not-apply-typeart --typeart-stack=true --typeart-filter=true --typeart-filter-implementation=acg --typeart-filter-cg-file=%p/26_malformed_cg.mcg 2>&1 | %filecheck %s
 // clang-format on
 
+// REQUIRES: not
+
 // comma is missing between nodes
 // CHECK: Fatal
 
